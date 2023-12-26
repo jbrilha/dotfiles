9bc7390acdfdd886c501e15b5dcd4d50b88833b3
jbrilha <j.brilha@campus.fct.unl.pt>
2023-12-26 20:52:52 +0000

    Added ghost-text to nvim-cmp

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,6 +1,6 @@
 return {
     'hrsh7th/nvim-cmp',
-    -- event = 'InsertEnter',
+    event = { 'InsertEnter', 'CmdLineEnter' },
     dependencies = {
         'L3MON4D3/LuaSnip',
         'hrsh7th/cmp-nvim-lsp',
@@ -19,13 +19,13 @@ return {
         -- luasnip.config.setup({})
 
         require('luasnip.loaders.from_vscode').lazy_load()
-
         cmp.setup({
             mapping = cmp.mapping.preset.insert({
                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete({}),
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
             }),
             snippet = {
                 expand = function(args)
@@ -40,6 +40,9 @@ return {
                 { name = 'buffer' },
                 { name = 'path' },
             }),
+            experimental = {
+                ghost_text = true
+            }
         })
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
