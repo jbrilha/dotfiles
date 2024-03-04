098fd8b865298dde6149b7cee2ddbb3b3706381d
jbrilha <j.brilha@campus.fct.unl.pt>
2024-03-04 15:11:56 +0000

    slight cleanup and added jdtls support

--- .config/nvim/lua/plugins/mason-null-ls.lua
+++ .config/nvim/lua/plugins/mason-null-ls.lua
@@ -1,45 +1,50 @@
 return {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "williamboman/mason.nvim",
-                                    "nvimtools/none-ls.nvim",
-    },
-    config = function()
-        require("mason").setup()
-        local null_ls = require("null-ls")
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	config = function()
+		require("mason").setup()
+		local null_ls = require("null-ls")
 
-        require("mason-null-ls").setup({
-            ensure_installed = { "stylua", "prettier", "black", "isort",
-                "clang_format", "eslint_d", "flake8"
-            },
-            automatic_installation = true,
-            handlers = {
-                function() end,
-                stylua = function(source_name, methods)
-                    null_ls.register(null_ls.builtins.formatting.stylua)
-                end,
-            }
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"black",
+				"isort",
+				"clang_format",
+				-- "eslint_d",
+				-- "flake8",
+			},
+			automatic_installation = true,
+			handlers = {
+				function() end,
+				stylua = function(source_name, methods)
+					null_ls.register(null_ls.builtins.formatting.stylua)
+				end,
+			},
+		})
 
-        })
-
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.clang_format.with({
-                    extra_args = {
-                        "-style=file:" .. vim.fn.expand("~/.local/share/nvim/mason/bin/.clang-format")
-                        -- Alternatively, place .clang.format in ~/ and delete everything after -style=file
-                    }
-                }),
-                null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.diagnostics.flake8,
-                -- null_ls.builtins.completion.spell,
-            },
-        })
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.clang_format.with({
+					extra_args = {
+						"-style=file:" .. vim.fn.expand("~/.local/share/nvim/mason/bin/.clang-format"),
+						-- Alternatively, place .clang.format in ~/ and delete everything after -style=file
+					},
+				}),
+				-- null_ls.builtins.diagnostics.eslint_d,
+				-- null_ls.builtins.diagnostics.flake8,
+				-- null_ls.builtins.completion.spell,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
 -- return {}
