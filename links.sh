a16633d8ed822206a00c1fdf075f981ba0d84da2
jbrilha <j.brilha@campus.fct.unl.pt>
2024-03-18 15:13:40 +0000

    chore: Cleaned up symlink commands

--- links.sh
+++ links.sh
@@ -1,11 +1,12 @@
-ln -s ~/.dotfiles/.config/nvim ~/.config/
-ln -s ~/.dotfiles/.config/navi ~/.config/
-ln -s ~/.dotfiles/.config/mdless ~/.config/
-ln -s ~/.dotfiles/.config/fastfetch ~/.config/
-ln -s ~/.dotfiles/.config/ranger ~/.config/
+ln -s ~/.dotfiles/.config/* ~/.config/
 
+ln -s ~/.dotfiles/.scripts ~/
 ln -s ~/.dotfiles/.zshrc ~/
 ln -s ~/.dotfiles/.tmux.conf ~/
 ln -s ~/.dotfiles/.tmux ~/
 ln -s ~/.dotfiles/iterm ~/
 ln -s ~/.dotfiles/.p10k.zsh ~/
+
+sudo ln -s ~/.scripts/* /usr/local/bin/
+# sudo ln -s ~/.scripts/ginit /usr/local/bin/
+# sudo ln -s ~/.scripts/gbranch /usr/local/bin/
