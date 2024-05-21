bafb4f0a1d8b15d6a827a61177a7773a9ad445e0
jbrilha <j.brilha@campus.fct.unl.pt>
2024-05-21 16:57:04 +0100

    refactor: binds, aliases and exports

--- links.sh
+++ links.sh
@@ -1,7 +1,11 @@
 ln -s ~/.dotfiles/.config/* ~/.config/
 
 ln -s ~/.dotfiles/.scripts ~/
+ln -s ~/.dotfiles/.binds ~/
+ln -s ~/.dotfiles/.aliases ~/
+ln -s ~/.dotfiles/.exports ~/
 ln -s ~/.dotfiles/.zshrc ~/
+ln -s ~/.dotfiles/.binds ~/
 ln -s ~/.dotfiles/.tmux.conf ~/
 ln -s ~/.dotfiles/.tmux ~/
 ln -s ~/.dotfiles/iterm ~/
