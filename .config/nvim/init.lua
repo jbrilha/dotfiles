cf43f88c629fe700d68ef898b70243e98579d58e
jbrilha <j.brilha@campus.fct.unl.pt>
2024-08-06 21:07:18 +0100

    chore: remove some plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,18 +2,23 @@ local vim = vim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("config.options")
 require("config.keymaps")
+
 require("lazy").setup("plugins")
 
-vim.cmd.colorscheme "catppuccin-mocha"
+vim.opt.termguicolors = true
+
+-- vim.cmd.colorscheme("neon")
+-- vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("bluloco")
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,14 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
+        tag = "0.1.6",
         dependencies = { "nvim-lua/plenary.nvim", "sharkdp/fd", "BurntSushi/ripgrep" },
         config = function()
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files)
             vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+            vim.keymap.set('n', '<leader>fb', builtin.buffers)
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags)
         end
     },
     {
@@ -14,6 +16,18 @@ return {
         config = function ()
             local telescope = require("telescope")
             telescope.setup({
+                defaults = {
+                    -- Default configuration for telescope goes here:
+                    -- config_key = value,
+                    mappings = {
+                        i = {
+                            -- map actions.which_key to <C-h> (default: <C-/>)
+                            -- actions.which_key shows the mappings for your picker,
+                            -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                            ["<C-h>"] = "which_key"
+                        }
+                    }
+                },
                 extensions = {
                     ["ui-select"] = require("telescope.themes").get_dropdown{}
                 }
