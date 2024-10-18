43be4dd8c77d3584676b2eca244443f300d4b48c
jbrilha <j.brilha@campus.fct.unl.pt>
2024-10-18 10:00:30 +0100

    refactor: lazy manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,24 +1,8 @@
-local vim = vim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("config.options")
 require("config.keymaps")
 
-require("lazy").setup("plugins")
-
-vim.opt.termguicolors = true
+require("config.lazy")
 
 -- vim.cmd.colorscheme("neon")
--- vim.cmd.colorscheme("catppuccin-mocha")
-vim.cmd.colorscheme("bluloco")
+vim.cmd.colorscheme("catppuccin-mocha")
+-- vim.cmd.colorscheme("bluloco")
