8f935dca6a24a7bdad3025ed01c28f3ca3bbe588
jbrilha <j.brilha@campus.fct.unl.pt>
2024-12-10 13:13:57 +0000

    chore: hide annoying files

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -216,11 +216,12 @@ local neotree = {
 						--".gitignored",
 					},
 					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-						--".DS_Store",
+						".DS_Store",
 						--"thumbs.db"
 					},
 					never_show_by_pattern = { -- uses glob style patterns
 						--".null-ls_*",
+                        "*.class"
 					},
 				},
 				follow_current_file = {
