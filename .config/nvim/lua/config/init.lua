8acea98417e30708e62f15ca982e925d2468a294
jbrilha <j.brilha@campus.fct.unl.pt>
2024-12-12 16:07:06 +0000

    chore: move signs to init

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -2,3 +2,8 @@ require("config.options")
 require("config.keymaps")
 require("config.commands")
 require("config.lazy")
+
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
