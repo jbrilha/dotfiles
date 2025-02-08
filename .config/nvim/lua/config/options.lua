6c85d20fec87b86958e71fafb3dc03aeebbc2412
jbrilha <j.brilha@campus.fct.unl.pt>
2025-02-08 23:49:40 +0000

    chore: add lsp_lines and move diagnostic stuff into options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,4 +12,25 @@ vim.o.undofile = true
 
 vim.diagnostic.config({
     severity_sort = true,
+    virtual_text = true, -- default but here for clarity
+    virtual_lines = false, -- start with lsp_lines disabled
+    signs = {
+        text = {        -- this disables gutter icons
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
+        },
+        numhl = { -- and this colors the line numbers according to the line diagnostic :) neat
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+            [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+        },
+    },
 })
+
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
